a9a9b6b7f8e67841ffa12c354a381df0e4287622
bglmmz <bglmmz@hotmail.com>
2019-10-17 11:16:39 +0800

    platon account unit test

--- tests/cmd/account/test_account.py
+++ tests/cmd/account/test_account.py
@@ -8,9 +8,11 @@ from common.connect import runCMDBySSH
 from conf.settings import NODE_FILE
 from environment import test_env_impl
 
+
+# py.test tests/cmd/account/test_account.py -s --nodeFile "deploy/4_node.yml" --accountFile "deploy/accounts.yml" --initChain --startAll
+
 class AccountEnv:
     __slots__ = ('remotePwdFile', 'remoteAccountAddress', 'remoteAccountFile', 'remoteKeyFile')
-
 
 @pytest.fixture(scope='module',autouse=False)
 def account_env(global_test_env):
@@ -44,53 +46,77 @@ def account_env(global_test_env):
 
 
 '''
-新建账号，不指定密码文件
-'''
-def test_account_new(account_env):
-    node, env = account_env
-
-
-    #returnList = runCMDBySSH(node.ssh, "find {} -name UTC--* | wc - l".format(node.remoteKeystoreDir))
-    #oldCounts = int(returnList[0].strip())
-
-    returnList = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
-    oldCounts = len(returnList) -1
-
-    runCMDBySSH(node.ssh, "{} account new --datadir {}  --keystore {}".format(node.remoteBinFile, node.remoteDataDir, node.remoteKeystoreDir), "88888888", "88888888")
-
-    #returnList2 = runCMDBySSH(node.ssh, "find {} -name UTC--* | wc - l".format(node.remoteKeystoreDir))
-    #assert  oldCounts + 1 == int(returnList2[0].strip())
-
-    returnList2 = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
-    newCounts = len(returnList2) -1
-    assert  oldCounts + 1 == newCounts
-
-
-'''
 新建账号，指定密码文件
 '''
 def test_account_new_with_pwd_file(account_env):
-    # env = global_test_env
-    # node = env.get_rand_node()
-
     node, env = account_env
-
-    #returnList = runCMDBySSH(node.ssh, "find {} -name UTC--* | wc - l".format(node.remoteKeystoreDir))
-    #oldCounts = int(returnList[0].strip())
     returnList = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
     oldCounts = len(returnList) - 1
 
-    # remotePwdFile = node.remoteDeployDir + "/password.txt"
-    # node.uploadFile("./deploy/keystore/password.txt", remotePwdFile)
-    # runCMDBySSH(node.ssh, "{} account new --datadir {}  --keystore {} --password {}".format(node.remoteBinFile, node.remoteDataDir, node.remoteKeystoreDir, remotePwdFile))
     runCMDBySSH(node.ssh, "{} account new --datadir {}  --keystore {} --password {}".format(node.remoteBinFile,
                                                                                             node.remoteDataDir,
                                                                                             node.remoteKeystoreDir,
                                                                                             env.remotePwdFile))
 
-    #returnList2 = runCMDBySSH(node.ssh, "find {} -name UTC--* | wc - l".format(node.remoteKeystoreDir))
-    #assert  oldCounts + 1 == int(returnList2[0].strip())
     returnList2 = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
     newCounts = len(returnList2) - 1
     assert oldCounts + 1 == newCounts
 
+
+
+'''
+修改账号密码，指定keystore
+'''
+def test_account_update(account_env):
+    node, env = account_env
+    runCMDBySSH(node.ssh, "{} account update {} --keystore {}".format(node.remoteBinFile, env.remoteAccountAddress, node.remoteKeystoreDir), "88888888", "88888888", "88888888")
+    pass
+
+
+'''
+导入账号，不指定密码文件，指定datadir
+'''
+def test_account_import(account_env):
+    # env = global_test_env
+    # node = env.get_rand_node()
+
+    node, env = account_env
+
+    returnList = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
+    for i in range(len(returnList)):
+        print("序号：%s   值：%s" % (i + 1, returnList[i]))
+    oldCounts = len(returnList) - 1
+
+    remoteKeyFile = node.remoteKeystoreDir + "/key.pri"
+    node.uploadFile("./deploy/key.pri", remoteKeyFile)
+
+    log.info("import key, {}:{}".format(node.host, node.port))
+    runCMDBySSH(node.ssh, "{} account import {} --datadir {}".format(node.remoteBinFile, remoteKeyFile, node.remoteDataDir), "88888888", "88888888")
+
+    returnList2 = runCMDBySSH(node.ssh, "{} account list --datadir {}".format(node.remoteBinFile, node.remoteDataDir))
+    for i in range(len(returnList)):
+        print("序号：%s   值：%s" % (i + 1, returnList[i]))
+
+    newCounts = len(returnList2) - 1
+
+
+    assert oldCounts + 1 == newCounts
+
+
+'''
+查看账号，指定datadir
+'''
+def test_account_list(account_env):
+    # env = global_test_env
+    # node = env.get_rand_node()
+
+    node, env = account_env
+
+    returnList1 = runCMDBySSH(node.ssh, "platon account list --datadir {}".format(node.remoteDataDir))
+
+    counts1 = len(returnList1) - 1
+
+    returnList2 = runCMDBySSH(node.ssh, "platon account list --keystore {}".format(node.remoteKeystoreDir))
+    counts2 = len(returnList2) - 1
+
+    assert  counts1 == counts2
